a391225a6a8ab9c02036ce3da12b4e727330e03d
liangzilixueha <542876162@qq.com>
2023-05-04 21:28:56 +0800

    增加：首页（仅骨架）

--- app/src/main/java/com/wifi_camera/mylibrary/Home.java
+++ app/src/main/java/com/wifi_camera/mylibrary/Home.java
@@ -3,12 +3,23 @@ package com.wifi_camera.mylibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class Home extends AppCompatActivity {
+
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        获得id();
+        //设置fragment为默认界面
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomeFragment()).commit();
+    }
+
+    private void 获得id() {
+        frameLayout = findViewById(R.id.frameLayout);
     }
 }
---
+++ app/src/main/java/com/wifi_camera/mylibrary/HomeFragment.java
@@ -0,0 +1,31 @@
+package com.wifi_camera.mylibrary;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
